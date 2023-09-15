2d4b789989863e5a216d763a7a95fde55d2d9302
ema1117 <129602709+ema1117@users.noreply.github.com>
2023-09-15 10:43:59 -0300

    carrousel
    se agregaron imagenes mas grandes y se modificarion algunos estilos

--- allgamer/src/componentes/Carrousel/Carrousel.jsx
+++ allgamer/src/componentes/Carrousel/Carrousel.jsx
@@ -3,36 +3,50 @@ import './carrousel.css'
 
 const Carrousel = () => {
   return (
-<div id="carouselExampleIndicators" class="carousel slide">
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
-  </div>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="../../src/assets/img/carrousel-notebook-asusi5.png" class="d-block w-100" alt="..."/>
+
+<div className="container text-center my-3 mt-5"> 
+      
+      <h2><span className='letraRoja'>@allgamers</span> en Instagram</h2>  
+
+  <div className='w-75 m-auto bordeRosaCards'>     
+    <div id="carouselExampleIndicators" className="carousel slide">
+      <div className="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active bgOscuro" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2" className='bgOscuro'></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3" className='bgOscuro'></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"className='bgOscuro' ></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4" aria-label="Slide 4"className='bgOscuro' ></button>
+      </div>
+      <div className="carousel-inner">
+        <div className="carousel-item active">
+          <img src="../../src/assets/img/carrousel-notebook-lenovo-i3.jpg" className="d-block w-100" alt="..."/>
+        </div>
+        <div className="carousel-item">
+          <img src="../../src/assets/img/carrousel-notebook-dell-i5.jpg" className="d-block w-100" alt="..."/>
+        </div>
+        <div className="carousel-item">
+          <img src="../../src/assets/img/CARROUSEL-notebook-DELL-I3.jpg" className="d-block w-100" alt="..."/>
+        </div>
+        <div className="carousel-item">
+          <img src="../../src/assets/img/carrousel-notebook-hp-i3.png" className="d-block w-100" alt="..."/>
+        </div>
+        <div className="carousel-item">
+          <img src="../../src/assets/img/carrousel-notebook-asus-i5.png" className="d-block w-100" alt="..."/>
+        </div>
+      </div>
+      <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+        <span className="flechaIzquierda" aria-hidden="true"></span>
+        <span className="visually-hidden">Previous</span>
+      </button>
+      <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+        <span className="flechaDerecha" aria-hidden="true"></span>
+        <span className="visually-hidden">Next</span>
+      </button>
     </div>
-    <div class="carousel-item">
-      <img src="../../src/assets/img/carrousel-notebook-Bango-i5.jpg" class="d-block w-100" alt="..."/>
+
     </div>
-    <div class="carousel-item">
-      <img src="../../src\assets\img\CARROUSEL-notebook-DELL-I3.jpg" class="d-block w-100" alt="..."/>
-    </div>
-    <div class="carousel-item active">
-      <img src="../../src\assets\img\carrousel-notebook-hp-i3.png" class="d-block w-100" alt="..."/>
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
+  </div>  
+
   ) 
 }
 export default Carrousel
