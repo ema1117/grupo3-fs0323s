05c9798d840a19578ffc1c2b1253d9f853f23c11
Álvaro Castaño <castalvar_7@hotmail.com>
2023-10-20 20:06:11 -0300

    componentes dia y pie realizados

--- allgamer/src/componentes/Dia/Dia.jsx
+++ allgamer/src/componentes/Dia/Dia.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import './dia.css'
 
 const Dia = () => {
-  return (
-    <div>Dia</div>
-  )
-}
+  return ( 
+  <div className='w-75 m-auto fondoDia d-flex justify-content-center align-items-end mt-5 gap-1 pb-2'>
+  <div className='d-flex flex-column flex-lg-row align-items-center gap-1'>
+    <p className='letraRosa m-0'>11/11 preparate para disfrutar tu dia a lo grande...</p>
+    <a className='vinculo' href="">Mas Informacion..</a>
+  </div>
+</div>
+    )
+
+  }
 
 export default Dia
--- allgamer/src/componentes/Pie/Pie.jsx
+++ allgamer/src/componentes/Pie/Pie.jsx
@@ -2,7 +2,54 @@ import React from 'react'
 
 const Pie = () => {
   return (
-    <div>Pie</div>
+    <div className='d-flex flex-column flex-lg-row justify-content-center align-items-lg-start gap-5 mt-5 '> 
+      <div>
+        <a href="">
+          <img src="../../src/assets/img/logo.png" alt="" />
+        </a>
+      </div>  
+
+
+      <div className='d-flex gap-4 letraRosa justify-content-center'>
+        <div className='text-start'>
+          <ul className="text-decoration-none list-unstyled ">
+            <li><a className="text-decoration-none colorRosa" href="#">Pc's Armadas</a></li>
+            <li><a className="text-decoration-none colorRosa" href="#">Placas de video</a></li>
+            <li><a className="text-decoration-none colorRosa" href="#">Almacenamiento</a></li>
+            <li><a className="text-decoration-none colorRosa" href="#">MotherBoards</a></li>
+            <li><a className="text-decoration-none colorRosa" href="#">Memoria Ram</a></li>
+            <li><a className="text-decoration-none colorRosa" href="#">Fuentes</a></li>
+            <li><a className="text-decoration-none colorRosa" href="#">Perifericos</a></li>
+            <li><a className="text-decoration-none colorRosa" href="#">Monitores</a></li>
+            <li><a className="text-decoration-none colorRosa" href="#">Otros</a></li>
+          </ul>
+        </div>
+
+        <div className='text-start'>
+          <a className='text-decoration-none colorRosa' href="">Arma tu PC</a>
+        </div>  
+
+        <div className='text-start'>
+          <ul className="list-style-none  list-unstyled">
+            <li><a className="text-decoration-none colorRosa" href="#">Nintendo</a></li>
+            <li><a className="text-decoration-none colorRosa" href="#">Playstation</a></li>
+            <li><a className="text-decoration-none colorRosa" href="#">Xbox</a></li>
+            <li><a className="text-decoration-none colorRosa" href="#">Sega</a></li>
+          </ul>
+        </div>  
+
+        <div className='text-start'>
+          <a className='text-decoration-none colorRosa' href="">Marcas</a>
+        </div>  
+
+        <div className='text-start'>
+          <a className='text-decoration-none colorRosa' href="">Sobre Nosotros</a>
+        </div>  
+      
+      </div>
+
+
+    </div>
   )
 }
 
