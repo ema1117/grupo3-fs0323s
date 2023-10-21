51abb65168cfdf8916a41b4e000dd2d0d6390913
ema1117 <129602709+ema1117@users.noreply.github.com>
2023-10-21 12:09:52 -0300

    responsive img dia
    se modifico tamaño de imagen en componente dia para apaptarla mejor al contenedor

--- allgamer/src/componentes/Dia/Dia.jsx
+++ allgamer/src/componentes/Dia/Dia.jsx
@@ -3,7 +3,7 @@ import './dia.css'
 
 const Dia = () => {
   return ( 
-  <div className='w-75 m-auto fondoDia d-flex justify-content-center align-items-end mt-5 gap-1 pb-2'>
+  <div className='w-75 m-auto border fondoDia d-flex justify-content-center align-items-end mt-5 gap-1 pb-2'>
   <div className='d-flex flex-column flex-lg-row align-items-center gap-1'>
     <p className='letraRosa m-0'>11/11 preparate para disfrutar tu dia a lo grande...</p>
     <a className='vinculo' href="">Mas Informacion..</a>
